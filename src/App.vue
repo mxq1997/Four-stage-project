a1737f96f3c6ca070da3a3760b0c6734f8ed45b7
mxq <1179919598@qq.com>
2020-07-13 21:34:40 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div style="width:1080px;margin:0 auto;">
+      <Banner :banners="banners" :da="2000"></Banner>
+    </div>
+   
+  </div>
+</template>
+
+<script>
+ import Banner from './components/Banner.vue'
+export default {
+  components:{
+    Banner:Banner,
+  },
+  data(){
+    return {
+      banners:[
+        {
+          url:require('../src/assets/images/banner1.jpg'),
+          link:'https://www.baidu.com'
+        },
+        {
+          url:require('../src/assets/images/banner2.jpg'),
+          link:'https://www.baidu.com'
+        },
+        {
+          url:require('../src/assets/images/banner3.jpg'),
+          link:'https://www.baidu.com'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
